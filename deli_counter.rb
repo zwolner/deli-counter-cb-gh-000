e6fa649cc8374db153c4cfccdcf6a495a27c7d8d
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 13:27:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(katz_deli=[])
   if katz_deli.length == 0
     return "The line is currently empty."
   else
-    katz_deli.each.with_index {|val, index| return "#{val} is ##{index+1} in line"
+    katz_deli.each.with_index {|val, index| return "#{val} is ##{index+1} in line"}
   end
 end
