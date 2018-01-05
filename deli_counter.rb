16544418f02f8cb11aa8a7f8b5bdadf418a4f82d
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 13:57:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli=[], "name")
+def take_a_number(katz_deli, learn)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli(name)} in line."
 end
