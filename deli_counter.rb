c91d6ab9bc845bc2fefc90b73a0025c6ba1aeb0b
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 14:04:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 # Write your code here.
 katz_deli=[]
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
@@ -9,7 +10,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli=[], name)
+def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
 end
