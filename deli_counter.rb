f550b0f093725e7a76398de9a0b1d9f53f3e9ef7
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 13:31:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(katz_deli=[])
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + katz_deli.each.with_index {|val, index| return "#{val} #{index+1}."}
+    puts "The line is currently: " + katz_deli.each.with_index {|val, index| return "#{val}. #{index+1}"}
   end
 end
